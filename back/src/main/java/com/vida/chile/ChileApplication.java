f26f6aa0fd2b06f2f5d757742069f021e25254d7
cedricnator <c.kirmayr01@ufromail.cl>
2024-09-25 11:24:17 -0300

    feat[back]: Add initial project files

---
+++ back/src/main/java/com/vida/chile/ChileApplication.java
@@ -0,0 +1,13 @@
+package com.vida.chile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChileApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/vida/chile/ChileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vida.chile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
