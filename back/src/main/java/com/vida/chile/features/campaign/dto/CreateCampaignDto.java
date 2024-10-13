755b3c07ca81a02fa34fe8b19c2902b5aae5550d
cedricnator <c.kirmayr01@ufromail.cl>
2024-10-13 09:36:30 -0300

    feat(back): Add CreateCampaignDto class

---
+++ back/src/main/java/com/vida/chile/features/campaign/dto/CreateCampaignDto.java
@@ -0,0 +1,12 @@
+package com.vida.chile.features.campaign.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateCampaignDto {
+   private String title;
+}
