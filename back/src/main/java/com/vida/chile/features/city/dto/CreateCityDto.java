aa07df3139fb8dd3450fd3e2cb4d1f7f7c29a521
cedricnator <c.kirmayr01@ufromail.cl>
2024-10-13 09:38:20 -0300

    feat(back): Add CreateCityDto class

---
+++ back/src/main/java/com/vida/chile/features/city/dto/CreateCityDto.java
@@ -0,0 +1,12 @@
+package com.vida.chile.features.city.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateCityDto {
+   private String name;
+}
