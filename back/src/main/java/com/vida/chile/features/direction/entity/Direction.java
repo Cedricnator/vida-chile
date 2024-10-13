a2e03634398de33d7e63ee179b5432eff0469f23
cedricnator <c.kirmayr01@ufromail.cl>
2024-10-13 09:53:11 -0300

    feat(back): Add Direction entity class

---
+++ back/src/main/java/com/vida/chile/features/direction/entity/Direction.java
@@ -0,0 +1,39 @@
+package com.vida.chile.features.direction.entity;
+
+import java.io.Serializable;
+
+import com.vida.chile.features.city.entity.City;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "direction")
+public class Direction implements Serializable {
+   @Id
+   @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="direction_sequence")
+   @SequenceGenerator(name="direction_sequence", sequenceName="direction_sequence", allocationSize=100)
+   private Long id;
+   
+   @ManyToMany
+   @JoinTable(
+      name = "direction_city",
+      joinColumns = @JoinColumn(name = "direction_id"),
+      inverseJoinColumns = @JoinColumn(name = "city_id")
+    )
+   private City city;
+    
+}
