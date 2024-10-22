6a8f369eaa4575ddeaafb067bca9897bc805774b
cedricnator <c.kirmayr01@ufromail.cl>
2024-10-22 18:33:01 -0300

    feat(mobile): Add toJson and fromJson methods to Campaign entity

--- mobile/lib/features/campaign/entity/campaign.entity.dart
+++ mobile/lib/features/campaign/entity/campaign.entity.dart
@@ -21,4 +21,26 @@ class Campaign {
   final String finalHour;
   final String startHour;
   final StateCampaign stateCampaign;
+
+
+  Campaign.fromJson(Map<String, dynamic> json)
+      : adress = json['adress'] as String,
+        city = json['city'] as String,
+        date = json['date'] as String,
+        donationCenterName = json['donationCenterName'] as String,
+        finalHour = json['finalHour'] as String,
+        startHour = json['startHour'] as String,
+        stateCampaign = StateCampaign.values.firstWhere(
+          (e) => e.toString() == 'StateCampaign.${json['stateCampaign']}',
+        );
+
+  Map<String, dynamic> toJson() => {
+        'adress': adress,
+        'city': city,
+        'date': date,
+        'donationCenterName': donationCenterName,
+        'finalHour': finalHour,
+        'startHour': startHour,
+        'stateCampaign': stateCampaign.toString().split('.').last,
+      };
 }
