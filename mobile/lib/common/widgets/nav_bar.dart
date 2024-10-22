d22eb7ce492025741dd71f9ea6ce5faeaea9141f
cedricnator <c.kirmayr01@ufromail.cl>
2024-10-22 18:06:50 -0300

    feat(mobile): Add NavBar widget for navigation

---
+++ mobile/lib/common/widgets/nav_bar.dart
@@ -0,0 +1,40 @@
+
+
+import 'package:flutter/material.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          ListTile(
+              title: const Text("Home"),
+              onTap: () {
+                Navigator.pushNamed(context, '/');
+              }),
+          ListTile(
+              title: const Text("Operativos"),
+              onTap: () {
+                Navigator.pushNamed(context, '/operativos');
+              }),
+          ListTile(
+            title: const Text("Preguntas Frecuentes"),
+            onTap: () {
+              Navigator.pushNamed(context, '/preguntas-frecuentes');
+            },
+          ),
+          //TODO: limit the access to be only from an campaign. delete this bypass
+          ListTile(
+            title: Text("Formulario"),
+            onTap: () {
+              Navigator.pushNamed(context, '/formulario');
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
