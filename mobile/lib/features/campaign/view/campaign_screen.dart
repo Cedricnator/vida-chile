ccc00a11ad44c0b98c1965bb4433e04910c2816c
cedricnator <c.kirmayr01@ufromail.cl>
2024-10-23 18:26:08 -0300

    feat(mobile): refactor campaign screen

--- mobile/lib/features/campaign/view/campaign_screen.dart
+++ mobile/lib/features/campaign/view/campaign_screen.dart
@@ -7,28 +7,80 @@ class CampaignPage extends StatelessWidget {
   const CampaignPage({super.key});
 
 
-  List<Card> _buildBloodCampaignCard() {
+Widget _buildStatusLabel(StateCampaign state) {
+  Color color;
+  String text;
+
+  print(state);
+  switch (state) {
+    case StateCampaign.current:
+      color = Colors.green;
+      text = 'en curso';
+      break;
+    case StateCampaign.soon:
+      color = const Color.fromARGB(255, 168, 154, 26);
+      text = 'proximo';
+      break;
+    case StateCampaign.finalized:
+      color = Colors.red;
+      text = 'finalizado';
+      break;
+    default:
+      color = Colors.grey;
+      text = 'cancelado';
+  }
+
+  return Container(
+    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+    decoration: BoxDecoration(
+      color: color,
+      borderRadius: BorderRadius.circular(12.0),
+    ),
+    child: Text(
+      text,
+      style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+    ),
+  );
+}
+
+  List<Container> _buildBloodCampaignCard() {
     List<Campaign> campaigns = CampaignRepository.loadCampaigns();
     return campaigns.map((campaign) {
-      return Card(
-        clipBehavior: Clip.antiAlias,
+      return Container(
+        margin: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey, width: 1.0),
+          borderRadius: BorderRadius.circular(22.0),
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(campaign.donationCenterName),
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                      "Horario: ${campaign.startHour} - ${campaign.finalHour} "),
-                  Text(campaign.city),
-                  Text(campaign.adress),
-                ],
+                    campaign.donationCenterName,
+                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+                  ),
+                  _buildStatusLabel(campaign.stateCampaign) 
+                ]
               ),
-            )),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10.0, 10.0, 16.0, 14.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Horario: ${campaign.startHour} - ${campaign.finalHour}"),
+                    Text(campaign.city),
+                    Text(campaign.adress),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       );
@@ -65,7 +117,7 @@ class CampaignPage extends StatelessWidget {
             child: GridView.count(
               crossAxisCount: 1,
               padding: const EdgeInsets.all(16.0),
-              childAspectRatio: 8.0 / 9.0,
+              childAspectRatio: 22.0 / 9.0,
               children: _buildBloodCampaignCard(),
             ),
           ),
