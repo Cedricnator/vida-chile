3d9572516fa17d654b83404e8f7ef3e4fef5e9bc
cedricnator <c.kirmayr01@ufromail.cl>
2024-11-21 17:26:00 -0300

    feat[frontend]: implement login form with validation and styling

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AUTH_REPOSITORY } from './core/auth/infrastructure/auth.provider';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field'
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,8 +16,10 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideHttpClient(withFetch()),
     provideAnimations(),
+    provideHttpClient(withFetch()),
 
     // Injection Tokens
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline'} },
     AUTH_REPOSITORY, provideAnimationsAsync()
   ]
 };
--- frontend/src/app/routes/login/login.component.ts
+++ frontend/src/app/routes/login/login.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../core/auth/presentation/auth.service';
+import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+  private readonly fb = inject(FormBuilder);
+
+  public loginForm: FormGroup = this.fb.group({
+    userName: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(5)]]
+  })
+
+  public login(){
+    if(!this.loginForm.valid) return;
+    this.authService.login(this.loginForm.value.userName, this.loginForm.value.password)
+    .subscribe({
+      next: () => {
+
+      },
+      error: (err) => {
+        console.log(err)
+      },
+      complete: () => {
+        this.router.navigateByUrl('/')
+      }
+    })
+  }
 
 }
