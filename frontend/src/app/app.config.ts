44ef3082ab6a97f63e93dadd0df9ce7b0ea39ad8
cedricnator <c.kirmayr01@ufromail.cl>
2024-11-21 19:00:56 -0300

    feat[frontend]: add injection tokens  in app.config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -8,6 +8,10 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field'
 import { routes } from './app.routes';
+import { BLOODBANK_REPOSITORY } from './core/bloodbank/infrastructure/bloodbank.provider';
+import { FORM_REPOSITORY } from './core/form/infrastructure/form.provider';
+import { OPERATIVE_REPOSITORY } from './core/operatives/infrastructure/operative.provider';
+import { USER_REPOSITORY } from './core/user/infrastructure/user.provider';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,10 +20,15 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideHttpClient(withFetch()),
     provideAnimations(),
+    provideAnimationsAsync(),
     provideHttpClient(withFetch()),
 
     // Injection Tokens
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline'} },
-    AUTH_REPOSITORY, provideAnimationsAsync()
+    AUTH_REPOSITORY, 
+    BLOODBANK_REPOSITORY,
+    FORM_REPOSITORY,
+    OPERATIVE_REPOSITORY,
+    USER_REPOSITORY
   ]
 };
