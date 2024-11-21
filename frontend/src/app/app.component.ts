2cef6e952dc827715010bc4cee2ff9912711c8d9
cedricnator <c.kirmayr01@ufromail.cl>
2024-11-21 16:49:12 -0300

    feat[frontend]: add basic setup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './shared/ui/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  template: `
+    <router-outlet />
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,6 +5,8 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AUTH_REPOSITORY } from './core/auth/infrastructure/auth.provider';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,8 +14,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withViewTransitions()), 
     provideClientHydration(),
     provideHttpClient(withFetch()),
+    provideAnimations(),
 
     // Injection Tokens
-    AUTH_REPOSITORY
+    AUTH_REPOSITORY, provideAnimationsAsync()
   ]
 };
